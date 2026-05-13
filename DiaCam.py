04c6a9a4deef5967452b5c671f2113ed02294622
nqtrung79 <nqtrung79@gmail.com>
2026-05-13 13:23:44 -0500

    Update DiaCam.py

--- DiaCam.py
+++ DiaCam.py
@@ -35,7 +35,7 @@ def translate_query(client_groq, query):
         return query
 
 def get_available_gemini_model(client_gemini):
-    return "gemini-1.5-flash"
+    return "gemini-2.5-flash"
 
 def fuzzy_food_search(df, keyword):
     """Tìm kiếm gần đúng trong dataframe"""
